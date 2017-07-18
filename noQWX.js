4cbaa7e4e9ed2d02a0b6949ce461820ab40604da
koziscool <kozjob@yahoo.com>
2017-07-18 02:53:43 -0500

    again

---
+++ noQWX.js
@@ -0,0 +1,7 @@
+var field = document.querySelector("input");
+field.addEventListener("keydown", function(event) {
+  if (event.keyCode == "Q".charCodeAt(0) ||
+      event.keyCode == "W".charCodeAt(0) ||
+      event.keyCode == "X".charCodeAt(0))
+    event.preventDefault();
+});
